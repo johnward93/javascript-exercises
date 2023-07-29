33389a10b605d3e0db3521c37f32e5a0f46213f6
John Ward <johnward127@gmail.com>
2023-07-29 22:48:29 +0100

    Complete exercise 01
    Updated js file and all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringHolder = ""
+    for (i = 0; i < num; i++) {
+        stringHolder = stringHolder.concat(string);
+    };
+    return stringHolder;
 };
 
 // Do not edit below this line
