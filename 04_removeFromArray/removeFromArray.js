47e216424660abef2c788ffa4b44a971b964eeb6
johnward127 <johnward127@gmail.com>
2023-08-01 11:33:59 +0100

    Start exercise 04
    Update removeFromArray.js, test to be complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, itemToBeRemoved) {
+    itemIndex = arr.indexOf(itemToBeRemoved);
+    arr.splice(itemIndex, 1);
+    return arr;
 };
 
 // Do not edit below this line
