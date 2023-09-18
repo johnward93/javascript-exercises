e016ba2c8d090d488d355c0a96863757f9d1db99
John Ward <johnward127@gmail.com>
2023-09-18 19:17:01 +0100

    Complete exercise 04_removeFromArray
    Worth noting; did not complete exercise in the same manner
    as the solution provided. Instead of adding arguments to a new
    array, I removed arguments from original array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function(arr, itemToBeRemoved) {
-    itemIndex = arr.indexOf(itemToBeRemoved);
-    arr.splice(itemIndex, 1);
-    return arr;
+const removeFromArray = function (arr, itemToBeRemoved) {
+    newArray = arr;
+    for (let i = 1; i < arguments.length; i++) {
+        if (newArray.includes(arguments[i])) {
+            itemIndex = newArray.indexOf(arguments[i]);
+            newArray.splice(itemIndex, 1);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
