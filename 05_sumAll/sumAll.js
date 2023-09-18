48ab418a76e93c6891ab16eb4a39f867140091d4
John Ward <johnward127@gmail.com>
2023-09-18 19:33:10 +0100

    Complete exercise 05_sumAll
    All tests pass, solution does not match one provided

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+    let final = 0;
+    if (typeof first != "number" || typeof second != "number" || first < 0 || second < 0) {
+        return "ERROR"
+    }
+    else if (first > second) {
+        for (let i = arguments[1]; i <= arguments[0]; i++) {
+            final = final + i;
+        }
+    }
+    else {
+        for (let i = arguments[0]; i <= arguments[1]; i++) {
+            final = final + i;
+        }
+    }
+    return final;
 
 };
 
