01c0ee0cb7f759b969592ad16f3fefa9e8f5b6fa
John Ward <johnward127@gmail.com>
2024-09-06 18:34:09 +0100

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x, y) {
+  num1 = parseInt(x);
+  num2 = parseInt(y);
+  return num1 + num2;  	
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  num1 = parseInt(x);
+  num2 = parseInt(y);
+  return num1 - num2; 
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	sumOfArray = x.reduce((total, current) => total + current, 0);
+  return sumOfArray;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  multipleOfArray = x.reduce((total, current) => total * current, 1);
+  return multipleOfArray;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(parseInt(x),parseInt(y));
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  arr = [];
+  while (x > 0) {
+    arr.push(x);
+    x--;
+  }
+  return multiply(arr);
 };
 
 // Do not edit below this line
